01eddade74986d92b9c330a1f3a16a5db5033c56
Md Tamjid Hossain <72858423+mtamjidhossain@users.noreply.github.com>
2021-08-14 17:13:55 +0600

    Update Solution.py

--- 0 - Mean, Median, and Mode/Solution.py
+++ 0 - Mean, Median, and Mode/Solution.py
@@ -1,1 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import numpy as np
+from scipy import stats
 
+size = int(input())
+lst= list(map(int, input().split())) #split splits numbers into list elements, map casts to int, list makes a list
+
+print(np.mean(lst))
+print(np.median(lst))
+print(int(stats.mode(lst)[0])) #[0] takes away all the details and keeps [x]
